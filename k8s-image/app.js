e4a2f80e68d542a76105a21b67e29450e43131f1
Armen96 <barsegyan96armen@gmail.com>
2021-10-31 14:01:27 +0400

    Image port number

--- k8s-image/app.js
+++ k8s-image/app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 const path = __dirname + '/views/';
 
 const router = express.Router();
