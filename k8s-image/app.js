32a5743b9253e58a999f215f7eb7c086d6f07135
Armen96 <barsegyan96armen@gmail.com>
2021-10-31 13:25:03 +0400

    K8 node image

---
+++ k8s-image/app.js
@@ -0,0 +1,29 @@
+//Load express module with `require` directive
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 8081;
+const path = __dirname + '/views/';
+
+const router = express.Router();
+
+
+router.use(function (req,res,next) {
+    console.log('/' + req.method);
+    next();
+});
+
+router.get('/', function(req,res){
+    res.sendFile(path + 'index.html');
+});
+
+router.get('/sharks', function(req,res){
+    res.sendFile(path + 'sharks.html');
+});
+
+app.use(express.static(path));
+app.use('/', router);
+
+app.listen(PORT, function () {
+    console.log(`app listening on port ${PORT}!`)
+})
