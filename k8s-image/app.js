f824dfb9839f8375a0d9f914b627f7ada4071783
Armen96 <barsegyan96armen@gmail.com>
2021-11-01 09:50:45 +0400

    Node image deployment

--- k8s-image/app.js
+++ k8s-image/app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 const path = __dirname + '/views/';
 
 const router = express.Router();
