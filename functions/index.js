d89e5a6280609c32d02592c109d71a959825abbf
Armen96 <barsegyan96armen@gmail.com>
2021-11-09 20:10:38 +0400

    Cloud SQL

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql');
 
 exports.validateTemperature = async (req, res) => {
     try {
@@ -13,4 +14,37 @@ exports.validateTemperature = async (req, res) => {
     } catch (error) {
         res.status(500).send(error);
     }
+};
+
+const TYPE = 'cloudsql';
+const PROJECT_ID = 'prod-329112';
+const REGION = 'us-central1';
+const SPANNER_INSTANCE_NAME = 'my-cloud-sql-instance-1';
+
+const pool = mysql.createPool({
+    connectionLimit : 1,
+    socketPath: `/${TYPE}/${PROJECT_ID}:${REGION}:${SPANNER_INSTANCE_NAME}`,
+    user: 'root',
+    password: '',
+    database: 'todos'
+});
+
+exports.getUsersHandler = function handler(req, res) {
+    pool.query(`SELECT * FROM users`, req.body, function (e, results) {
+        const response = JSON.stringify(results);
+        res.status(200).send(response)
+    });
+
+    //using pool instead of creating connection with function call
+    // pool.query(`SELECT * FROM users where id = ?`,
+    //     req.body.id, function (e, results) {
+    //         //made reply here
+    //     });
+};
+
+exports.createUserHandler = function handler(req, res) {
+    pool.query(`INSERT INTO users VALUES (2, 'John', 'Bolton')`, req.body, function (e, results) {
+        const response = JSON.stringify(results);
+        res.status(200).send(response)
+    });
 };
